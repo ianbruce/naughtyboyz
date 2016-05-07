4e15510ef7637dba9a31901165ef91613792dd6f
ianbruce <bruceiprofessional@gmail.com>
2016-05-07 15:00:57 -0700

    changed some things

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 data = pd.read_csv("data.csv", header=None)
 names = pd.read_csv("names.csv", header=None)
+
+def calculate_loss(partition, objective):
+	def compare_pair(pair):
+		ans = False
+		for part in partition:
+			return ans or (pair[0] in part and pair[1] in part)
+	total = ()			# find all the 
+	loss = 0
+	for part in partition:
+		total += part
+	every_pair = itertools.combinations(total, 2)
+	for pair in every_pair:
+		loss += objective(pair, compare_pair(pair))
+	return loss
+
+calculate_loss()
